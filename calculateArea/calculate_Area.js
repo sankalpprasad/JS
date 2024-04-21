1dc1362316b5440d7fd6feedd6469e994eedd05b
sankalpprasad <sankalp.prasad.in@iclou.com>
2024-04-21 09:53:27 -0400

    Function

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -5,5 +5,5 @@ length = parseFloat(document.getElementById('length').value);
 width = parseFloat(document.getElementById('width').value);
 
 let area = length * width;
-document.getElementById('result').innerText = 'The area of the rectangle is: ${area}';
+document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
